28bb089f13d1675190d9660dda1e10f1d814a828
paarthsaggarwal <79054391+paarthsaggarwal@users.noreply.github.com>
2022-04-10 08:59:12 +0100

    Update variables example.cpp

--- variables example.cpp
+++ variables example.cpp
@@ -9,18 +9,21 @@ int main()
 
     int age;
     age = 80;
+    
+    int ageofdeath;
+    ageofdeath = 100;
 
     string mom = "Rebecca";
     string father = "Jeff";
-    string pronounofbob = "his";
+    string pronounofbob = "His";
 
     cout  << "There was once a man named " << name << endl;
     cout  << "He was " << age << " years old" << endl;
     cout  << "He liked the name " << name << endl;
     cout  << "But did not like being old" << endl;
-    cout  << "My mom is " << mom << endl;
-    cout  << "My father is " << father << endl; 
-    cout  << pronounofbob << " death will happen in 2042" << endl;
+    cout  << pronounofbob << " mom is " << mom << endl;
+    cout  << pronounofbob << " father is " << father << endl; 
+    cout  << pronounofbob << " death will happen in 2042 at the age of " << ageofdeath << endl;
 
     return 0;
 }
