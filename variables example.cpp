5f67a8ef1bb24ca14418188e5841fc3e51354536
paarthsaggarwal <79054391+paarthsaggarwal@users.noreply.github.com>
2022-04-10 08:53:07 +0100

    Add files via upload

---
+++ variables example.cpp
@@ -0,0 +1,26 @@
+# include <iostream> 
+
+using namespace std;
+
+int main()
+
+{
+    string name = "Bob";
+
+    int age;
+    age = 80;
+
+    string mom = "Rebecca";
+    string father = "Jeff";
+    string pronounofbob = "his";
+
+    cout  << "There was once a man named " << name << endl;
+    cout  << "He was " << age << " years old" << endl;
+    cout  << "He liked the name " << name << endl;
+    cout  << "But did not like being old" << endl;
+    cout  << "My mom is " << mom << endl;
+    cout  << "My father is " << father << endl; 
+    cout  << pronounofbob << " death will happen in 2042" << endl;
+
+    return 0;
+}
